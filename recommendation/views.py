4c85b98c4bf54a1cf17e72514ba15676a2f93d43
John Lane <jlane@sportinginnovations.com>
2018-07-22 03:55:03 -0500

    Initial views for recommendation

--- recommendation/views.py
+++ recommendation/views.py
@@ -6,12 +6,15 @@
 """
 
 from django.contrib.auth.models import User
+from haystack.query import SearchQuerySet
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from recommendation.models import *
 from recommendation.serializers import *
 
 __all__ = ['EpisodeViewSet', 'HistorylistMovieViewSet', 'HistorylistShowViewSet', 'MovieViewSet', 'ShowViewSet',
-           'TraktSessionViewSet', 'UserViewSet', 'WatchlistMovieViewSet', 'WatchlistShowViewSet']
+           'TraktSessionViewSet', 'UserViewSet', 'WatchlistMovieViewSet',  'WatchlistShowViewSet']
 
 
 class EpisodeViewSet(viewsets.ModelViewSet):
@@ -28,12 +31,44 @@ class HistorylistMovieViewSet(viewsets.ModelViewSet):
     queryset = HistorylistMovie.objects.all()
     serializer_class = HistorylistMovieSerializer
 
+    @action(methods=['post'], detail=False)
+    def recommendation(self, request):
+        """Retrieve recommendation from movies in history
+
+        :param request:
+        :return:
+        """
+
+        if request:
+            pass
+
+        queryset = SearchQuerySet().models(HistorylistMovie).all()
+        serializer = HistorylistMovieSerializer(queryset, many=True)
+
+        return Response(serializer.data)
+
 
 class HistorylistShowViewSet(viewsets.ModelViewSet):
     """History list show view set instance
     """
     queryset = HistorylistShow.objects.all()
     serializer_class = HistorylistShowSerializer
+
+    @action(methods=['post'], detail=False)
+    def recommendation(self, request):
+        """Retrieve recommendation from shows in history
+
+        :param request:
+        :return:
+        """
+
+        if request:
+            pass
+
+        queryset = SearchQuerySet().models(HistorylistShow).all()
+        serializer = HistorylistShowSerializer(queryset, many=True)
+
+        return Response(serializer.data)
 
 
 class MovieViewSet(viewsets.ModelViewSet):
@@ -74,9 +109,41 @@ class WatchlistMovieViewSet(viewsets.ModelViewSet):
     queryset = WatchlistMovie.objects.all()
     serializer_class = WatchlistMovieSerializer
 
+    @action(methods=['post'], detail=False)
+    def recommendation(self, request):
+        """Retrieve recommendation from movies in watchlist
+
+        :param request:
+        :return:
+        """
+
+        if request:
+            pass
+
+        queryset = SearchQuerySet().models(WatchlistMovie).all()
+        serializer = WatchlistMovieSerializer(queryset, many=True)
+
+        return Response(serializer.data)
+
 
 class WatchlistShowViewSet(viewsets.ModelViewSet):
     """Watchlist show view set instance
     """
     queryset = WatchlistShow.objects.all()
     serializer_class = WatchlistShowSerializer
+
+    @action(methods=['post'], detail=False)
+    def recommendation(self, request):
+        """Retrieve recommendation from shows in watchlist
+
+        :param request:
+        :return:
+        """
+
+        if request:
+            pass
+
+        queryset = SearchQuerySet().models(WatchlistShow).all()
+        serializer = WatchlistShowSerializer(queryset, many=True)
+
+        return Response(serializer.data)
